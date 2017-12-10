210f9f3270b8852305fa20245427bafcbd63405f
Bartosz Gordon <bartosz.gordon@sealcode.org>
2017-12-10 17:48:57 +0100

    Setup the fetch action

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
-console.log("Hello, hna!");
+const program = require("commander");
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+require("http").globalAgent.maxSockets = 501;
+
+const { fetch } = require("./lib/actions");
+const { get_config } = require("./lib/utils");
+const config = get_config();
+
+program
+	.command("fetch <number>")
+	.alias("f")
+	.description(
+		"Fetch specified number or top articles from from given source"
+	)
+	.option(
+		"-s, --source <source>",
+		"Which source to fetch from",
+		config.default_source
+	)
+	.action(fetch);
+
+program.parse(process.argv);
