bfa85877a740543cf4edafc1046712ef36097f3d
Bartosz Gordon <bartosz.gordon@sealcode.org>
2017-12-16 14:58:27 +0100

    Add option to add single article by hand

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ mongoose.Promise = global.Promise;
 require("http").globalAgent.maxSockets = 501;
 const { start_db } = require("./lib/utils/db");
 
-const { fetch } = require("./lib/actions");
+const { fetch, add } = require("./lib/actions");
 const { get_config } = require("./lib/utils/misc");
 const config = get_config();
 
@@ -23,5 +23,13 @@ program
 		config.default_source
 	)
 	.action(fetch(db));
+
+program
+	.command("add")
+	.alias("a")
+	.description("Add single article, preferably outside of defined sources")
+	.option("-t, --title <title>", "Title of the article")
+	.option("-u, --url <url>", "URL of the article")
+	.action(add(db));
 
 program.parse(process.argv);
