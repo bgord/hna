4c841261c0e15358b07d05d685bdedcc7df418cc
Bartosz Gordon <bartosz.gordon@sealcode.org>
2017-12-19 13:53:18 +0100

    Clean tmp files

--- clean_tmp_files.js
+++ clean_tmp_files.js
@@ -3,13 +3,26 @@ const { unlink, readdir } = require("fs");
 const get_files = promisify(readdir);
 const delete_file = promisify(unlink);
 
+const flatten_arr = arr => [].concat(...arr);
+
 (async () => {
 	try {
-		const files_to_delete = await get_files("./tmp_files").then(files =>
-			files.filter(file => file.includes(".html"))
+		const directories = ["./tmp_files", "./newspapers"];
+		const extensions_to_delete = ["epub", "mobi", "html", "txt"];
+		const files_to_delete = await Promise.all(
+			directories.map(dir =>
+				get_files(dir).then(files =>
+					files.map(file => `${dir}/${file}`)
+				)
+			)
+		).then(arr =>
+			flatten_arr(arr).filter(file => {
+				const extension = file.split(".")[2];
+				return extensions_to_delete.indexOf(extension) !== -1;
+			})
 		);
 		console.log(files_to_delete);
-		files_to_delete.forEach(file => delete_file(`./tmp_files/${file}`));
+		files_to_delete.forEach(file_dir => delete_file(file_dir));
 	} catch (e) {
 		console.log(e.message);
 	}
