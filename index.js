04d618eb368386fc67d44bb2a777f9deaf15c198
Bartosz Gordon <bartosz.gordon@sealcode.org>
2017-12-30 15:44:01 +0100

    Refactor error handling

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ const { get_config } = require("./lib/utils/misc");
 const config = get_config();
 
 const db = start_db();
+
+const with_err_handling = func =>
+	async function(...args) {
+		try {
+			console.time("Finished in");
+			await func(...args);
+			await db.close();
+			console.timeEnd("Finished in");
+		} catch (e) {
+			console.log(e.message);
+			await db.close();
+			process.exit(1);
+		}
+	};
+
 program
 	.command("fetch <number>")
 	.alias("f")
@@ -24,7 +39,7 @@ program
 		"Which source to fetch from",
 		config.default_source
 	)
-	.action(fetch(db));
+	.action(with_err_handling(fetch(db)));
 
 program
 	.command("add")
@@ -32,7 +47,7 @@ program
 	.description("Add single article, preferably outside of defined sources")
 	.option("-t, --title <title>", "Title of the article")
 	.option("-u, --url <url>", "URL of the article")
-	.action(add(db));
+	.action(with_err_handling(add(db)));
 
 program
 	.command("review <number>")
@@ -43,7 +58,7 @@ program
 		"Type of articles to review",
 		config.review.default_query
 	)
-	.action(review(db));
+	.action(with_err_handling(review(db)));
 
 program
 	.command("list <number>")
@@ -54,18 +69,18 @@ program
 		"Match articles only with given query",
 		config.list.default_query
 	)
-	.action(list(db));
+	.action(with_err_handling(list(db)));
 
 program
 	.command("push <number>")
 	.alias("p")
 	.description("Send article to your Kindle device")
-	.action(push(db));
+	.action(with_err_handling(push(db)));
 
 program
 	.command("stats")
 	.alias("s")
 	.description("Display stats about your use of raok")
-	.action(stats(db));
+	.action(with_err_handling(stats(db)));
 
 program.parse(process.argv);
